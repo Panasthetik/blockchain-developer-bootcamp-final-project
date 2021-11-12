7e70b90997a996ea16a0e3a12d02539a1335c029
Panasthetik <jasonjagello@protonmail.com>
2021-11-12 12:11:19 -0500

    edit contract connector files

--- ethereum/exhibition.js
+++ ethereum/exhibition.js
@@ -1,5 +1,5 @@
 import web3 from './web3';
-import Exhibition from './build/Exhibition.json';
+import Exhibition from './build/contracts/Exhibition.json';
 
 const Address = (address) => {
     return new web3.eth.Contract (
--- ethereum/station.js
+++ ethereum/station.js
@@ -1,21 +1,16 @@
 import web3 from './web3';
-import CurationStation from './build/CurationStation.json';
+import CurationStation from './build/contracts/CurationStation.json';
 
 const instance = new web3.eth.Contract (
     CurationStation.abi,
 
+
 /*
-KOVAN:
+PASTE DEPLOYED GANACHE CONTRACT INSTANCE HERE:
 */
 
-    '0x0000000000000000000000000000000000000000'
+    '0x4D1460eCcead962770875eA3C7087006D9f180c7'
 
-/*
-ROPSTEN:
-
-/*
-    '0x0000000000000000000000000000000000000000'
-*/
     );
 
 export default instance;
